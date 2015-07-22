b5eab9d491eb5ff6cc7c6a2ddb888aee81b2b768
nearby <nearbycoder@gmail.com>
2015-07-21 21:53:51 -0500

    fix responsiveness

--- client/scripts/components/chatinput.jsx
+++ client/scripts/components/chatinput.jsx
@@ -76,8 +76,8 @@ var ChatInput = React.createClass({
 				<div className="row">
 					<div className="large-12 columns mainContainer">
 						<div id="userChat" className="small-12 large-12 columns userChat">
-							<div className="large-2 columns sideMessage"></div>
-							<textarea rows="1" ref="message" className="large-9 small-12 columns userMessage chat" onKeyDown={this.addMessage}/>
+							<div className="large-2 small-2 hide-on-small-only columns sideMessage"></div>
+							<textarea rows="1" ref="message" className="large-9 medium-12 small-12 columns userMessage chat" onKeyDown={this.addMessage}/>
 						</div>
 					</div>
 				</div>
