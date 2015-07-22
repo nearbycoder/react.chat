74ffebf2dc4c08be72cc941936af65bccc2ca798
nearby <nearbycoder@gmail.com>
2015-07-21 21:59:27 -0500

    fix responsiveness for landscape ipad mini

--- client/scripts/components/chatinput.jsx
+++ client/scripts/components/chatinput.jsx
@@ -76,7 +76,7 @@ var ChatInput = React.createClass({
 				<div className="row">
 					<div className="large-12 columns mainContainer">
 						<div id="userChat" className="small-12 large-12 columns userChat">
-							<div className="large-2 small-2 hide-on-small-only columns sideMessage"></div>
+							<div className="large-2 small-2 columns sideMessage"></div>
 							<textarea rows="1" ref="message" className="large-9 medium-12 small-12 columns userMessage chat" onKeyDown={this.addMessage}/>
 						</div>
 					</div>
