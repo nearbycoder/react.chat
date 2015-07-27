1f712dd4ca5dde125726a53d00dfda15e78445da
nearby <nearbycoder@gmail.com>
2015-07-26 20:46:53 -0500

    show colors to screen for color help

--- client/scripts/components/chatbox.jsx
+++ client/scripts/components/chatbox.jsx
@@ -129,8 +129,17 @@ var ChatBox = React.createClass({
 	componentDidUpdate: function(prevProps, prevState) {
 		//parse last message for links
 		var $elem = $( "code:last").html();
+		if($elem.includes("color can be set to")){
+			var colorMessage = [];
+			var colors = ["aliceblue","antiquewhite","aqua","aquamarine","azure","beige","bisque","black","blanchedalmond","blue","blueviolet","brown","burlywood","cadetblue","chartreuse","chocolate","coral","cornflowerblue","cornsilk","crimson","cyan","darkblue","darkcyan","darkgoldenrod","darkgray","darkgrey","darkgreen","darkkhaki","darkmagenta","darkolivegreen","darkorange","darkorchid","darkred","darksalmon","darkseagreen","darkslateblue","darkslategray","darkslategrey","darkturquoise","darkviolet","deeppink","deepskyblue","dimgray","dimgrey","dodgerblue","firebrick","floralwhite","forestgreen","fuchsia","gainsboro","ghostwhite","gold","goldenrod","gray","grey","green","greenyellow","honeydew","hotpink","indianred","indigo","ivory","khaki","lavender","lavenderblush","lawngreen","lemonchiffon","lightblue","lightcoral","lightcyan","lightgoldenrodyellow","lightgray","lightgrey","lightgreen","lightpink","lightsalmon","lightseagreen","lightskyblue","lightslategray","lightslategrey","lightsteelblue","lightyellow","lime","limegreen","linen","magenta","maroon","mediumaquamarine","mediumblue","mediumorchid","mediumpurple","mediumseagreen","mediumslateblue","mediumspringgreen","mediumturquoise","mediumvioletred","midnightblue","mintcream","mistyrose","moccasin","navajowhite","navy","oldlace","olive","olivedrab","orange","orangered","orchid","palegoldenrod","palegreen","paleturquoise","palevioletred","papayawhip","peachpuff","peru","pink","plum","powderblue","purple","red","rosybrown","royalblue","saddlebrown","salmon","sandybrown","seagreen","seashell","sienna","silver","skyblue","slateblue","slategray","slategrey","snow","springgreen","steelblue","tan","teal","thistle","tomato","turquoise","violet","wheat","white","whitesmoke","yellow","yellowgreen"];
+			colors.forEach(function(color){
+				colorMessage.push("<span style='color:"+ color +"'>"+ color +"</span>")
+			})
+			$elem = $elem + colorMessage.join(", ");
+		}
 		$elem = urlify($elem);
 		$( "code:last").html($elem);
+
 		if(this.color && !this.code){
 			$( "code:last").html('<span style="color:' + this.color +'">'+ $elem +'</span>');
 		}
